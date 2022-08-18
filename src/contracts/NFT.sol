e761e7af9293cb4d20d87a2b4d98f88273be1442
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-18 23:10:25 +0530

    market

--- src/contracts/NFT.sol
+++ src/contracts/NFT.sol
@@ -1,20 +1,27 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.8.4;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721URIStorage{
-    uint public tokenCount;
-
-    constructor() ERC721("Music NFT","MUS")
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+    
+    constructor(address marketplaceAddress) ERC721("NFT Market","MKT")
     {
-
+        contractAddress = marketplaceAddress ;
     }
 
-    function mint(string memory _tokenURI) public returns(uint)
+    function createToken(string memory tokenURI) public returns(uint)
     {
-        tokenCount++;
-        _safeMint(msg.sender,tokenCount);
-        _setTokenURI(tokenCount,_tokenURI);
-        return(tokenCount);
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true); //gives approval to the marketplace to transact tokens between users from another contract
+        return newItemId;
     }
 }
