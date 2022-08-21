07214ba2e16dfaef813c5e23982333cbc91436c2
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-21 11:17:11 +0530

    hooks

---
+++ src/hooks/GetContract.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useContract } from 'wagmi';
+import { useSigner } from 'wagmi';
+
+const GetContract = () => {
+
+    const{data:signer}=useSigner();
+
+    const contract = useContract({
+        addressOrName: '',
+        contractInterface: VotechainABI,
+        signerOrProvider: signer,
+      })
+
+    return contract;
+}
+ 
+export default GetContract;
