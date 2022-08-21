60e207106c086a6d1090852ee4183556462a8388
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-22 00:12:05 +0530

    loading state updated

--- src/App.js
+++ src/App.js
@@ -1,40 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-import {ethers} from "ethers";
-import { useState } from 'react';
-import MarketplaceAbi from '../src/contracts/ABIs/Marketplace.json'
-import MarketplaceAddress from '../src/contracts/Contract_Address/Marketplace-Address.json'
-import NFTAbi from '../src/contracts/ABIs/NFT.json'
-import NFTAddress from '../src/contracts/Contract_Address/NFTAddress.json'
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import { ethers } from "ethers";
+import { useState } from "react";
+import MarketplaceAbi from "../src/contracts/ABIs/Marketplace.json";
+import MarketplaceAddress from "../src/contracts/Contract_Address/Marketplace-Address.json";
+import NFTAbi from "../src/contracts/ABIs/NFT.json";
+import NFTAddress from "../src/contracts/Contract_Address/NFTAddress.json";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Create from "./components/Create";
+import ListedItems from "./components/ListedItems";
+import PurchasedItems from "./components/PurchasedItems";
 function App() {
-  const[loading, setLoading] = useState(true)
-  const[nft, setNFT] = useState({})
-  const[marketplace,setMarketplace] = useState({})
+  const [loading, setLoading] = useState(true);
+  const [nft, setNFT] = useState({});
+  const [marketplace, setMarketplace] = useState({});
 
-  const web3Handler = async() => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const signer = provider.getSigner()
-    loadContracts(signer)
-  } 
+  const web3Handler = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    loadContracts(signer);
+  };
 
-  const loadContracts = async(signer) =>{
-     const marketplace = new ethers.Contract(MarketplaceAddress.address, MarketplaceAbi.abi, signer)
-     setMarketplace(marketplace)
-     const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer)
-     setNFT(nft)
-     setLoading(false)
-  }
+  const loadContracts = async (signer) => {
+    const marketplace = new ethers.Contract(
+      MarketplaceAddress.address,
+      MarketplaceAbi.abi,
+      signer
+    );
+    setMarketplace(marketplace);
+    const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer);
+    setNFT(nft);
+    setLoading(false);
+  };
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path="/create" element={<VoterDetails/>}/>
-        <Route path="/listed-items" element={<CandidateDetails/>}/>
-        <Route path="/purchased-items" element={<CandidateList/>} />
-      </Routes>
-      </BrowserRouter>     
+        <div>
+          {loading ? (
+            <label>Loading...</label>
+          ) : (
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/create" element={<Create />} />
+              <Route path="/listed-items" element={<ListedItems />} />
+              <Route path="/purchased-items" element={<PurchasedItems />} />
+            </Routes>
+          )}
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
