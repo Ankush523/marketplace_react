c28fae20972033f12bc67d97abbde9ff369dc7e6
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-21 22:53:18 +0530

    ABIs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {ethers} from "ethers";
+import { useState } from 'react';
 
 function App() {
+
+  const[nft, setNFT] = useState({})
+  const[marketplace,setMarketplace] = useState({})
+
+  const web3Handler = async() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    loadContracts(signer)
+  } 
+
+  const loadContracts = async(signer) =>{
+     const marketplace = new ethers.Contract(MarketplaceAddress.address, MarketplaceAbi.abi, signer)
+     setMarketplace(marketplace)
+     const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer)
+     setNFT(nft)
+  }
   return (
     <div className="App">
      
--- src/hooks/GetContract.js
+++ src/hooks/GetContract.js
@@ -16,3 +16,5 @@ const GetContract = () => {
 }
  
 export default GetContract;
+
+
